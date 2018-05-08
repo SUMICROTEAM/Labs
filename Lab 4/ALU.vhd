5d09e29e8a2076c6f4cdf20bd5e26347c69177ff
HunterM982 <38116796+HunterM982@users.noreply.github.com>
2018-05-07 17:00:13 -0700

    ALU Starts

--- Lab 4/ALU.vhd
+++ Lab 4/ALU.vhd
@@ -33,9 +33,24 @@ architecture ALU_Arch of ALU is
 			shamt:	in std_logic_vector(4 downto 0);
 			dataout: out std_logic_vector(31 downto 0));
 	end component shift_register;
-
+	
+	signal AddSubRes,ShiftRes: std_logic_vector(31 downto 0);
 begin
-	-- Add ALU VHDL implementation here
+	AddSub: adder_subtracter PORT MAP(DataIn1,DataIn2,ALUCtrl(   ),ALUResult,);
+	
+	Shift: shift_register PORT MAP(DataIn1,ALUCtrl(   ),DataIn2,ALUResult)
+	
+	with ALUCtrl(4 downto 2) select 
+	ALUResult <= AddSubRes when "000",
+				 ShiftRes when "011",
+				 DataIn1 AND DataIn2 when "001",
+				 DataIn1 OR DataIn2 when "010",
+				 DataIn2 when "100",
+				 h(ZZZZZZZZ) when others;
+	
+	with ALUResult select
+	Zero <= '1' when h(00000000),
+			'0' when others;
 
 end architecture ALU_Arch;
 
