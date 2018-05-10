607c9a18ca38f56f8ef18f126f7eea6ba8361ea3
HunterM982 <38116796+HunterM982@users.noreply.github.com>
2018-05-10 09:50:00 -0700

    Added .s Test Driver
    vhdl testbench version to be done

---
+++ Lab 4/TesDriver.s
@@ -0,0 +1,19 @@
+.data
+
+
+.text
+
+addi t0,zero,5
+addi t1,zero,1234567
+add t2,t1,t0
+sub t3,t2,t1
+and t4,t3,t2
+andi t5,t4,0x8888888
+or t6,t5,t4
+ori t7,t6,0x12345678
+sll t8,t0
+slli t8,2
+srl t8,t0
+srli t8,3
+
+
