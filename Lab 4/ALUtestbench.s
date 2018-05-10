6201997adac06bf60b7c828e9d615516faedfba6
HunterM982 <38116796+HunterM982@users.noreply.github.com>
2018-05-10 14:12:39 -0700

    ALU Test File Ready to test
    both .s and .vhd are ready to be tested against eachother

---
+++ Lab 4/ALUtestbench.s
@@ -0,0 +1,14 @@
+.text
+addi t0,zero,2
+addi t1,zero,1245
+add t2,t1,t0
+sub t3,t1,t2
+and t4,t3,t2
+andi t5,t4,63
+or t6,t5,t4
+ori a0,t6,568
+sll a1,a0,t0
+slli a2,a1,2
+srl a3,a2,t0
+srli a4,a3,2
+
--- Lab 4/tALU.vhd
+++ Lab 4/tALU.vhd
@@ -45,70 +45,85 @@ BEGIN
 
 	tb : PROCESS
 	
-	variable t0,t1,t2,t3,t4,t5,t6,t7,t8 : std_logic_vector(31 downto 0) := '0';
+	variable t0,t1,t2,t3,t4,t5,t6,t7,t8 : std_logic_vector(31 downto 0) := x"00000000";
 	
 	BEGIN
 		-- Wait 100 ns for global reset to finish
 		wait for 100 ns;
-
-		-- Test add
-		datain_a <= ;
-		datain_b <= ;
-		control  <= "00000"; -- opcode "000" for add/sub  funct2 "00" for add
-		wait for 20 ns; 
 		-- Test addi
 		control <= "00001"; -- opcode "000" for add/sub  funct2 "01" for addi
-		datain_a <=       ;
-		datain_b <= h(ZZZZZZZZ); --Expect 0x
+		datain_a <= x"00000000";
+		datain_b <= x"00000002";
+		t0 =: result; -- addi t0,zero,2   Expect t0 = 00000002
+		control <= "00001"; -- opcode "000" for add/sub  funct2 "01" for addi
+		datain_a <= x"00000000";
+		datain_b <= x"000004dd";
+		t1 =: result; -- addi t1,zero,1245  Expect t1 = 000004dd
 		wait for 20 ns; 
+		-- Test add 
+		datain_a <= t1;
+		datain_b <= t0;
+		control  <= "00000"; -- opcode "000" for add/sub  funct2 "00" for add
+		t2 =: result; -- add t2,t1,t0   Expect 000004df
+		wait for 20 ns; 
+
 		-- Test Sub
 		control <= "00010"; -- opcode "000" for add/sub  funct2 "10" for sub
-		datain_a <=       ;
-		datain_b <= h(ZZZZZZZZ);
+		datain_a <= t1;
+		datain_b <= t2;
+		t3 =: result; -- sub t3,t2,t1 Expect fffffffe
 		wait for 20 ns; 
 		-- Test and
 		control <= "00100"; -- opcode "001" for and/i  funct2 "00" for and
-		datain_a <=       ;
-		datain_b <= h(ZZZZZZZZ);
+		datain_a <= t3;
+		datain_b <= t2;
+		t4 =: result -- and t4,t3,t2   Expect 000004de
 		wait for 20 ns; 
 		-- Test andi
 		control <= "00110"; -- opcode "001" for and/i  funct2 "10" for andi
-		datain_a <=       ;
-		datain_b <= h(ZZZZZZZZ);
+		datain_a <= t4;
+		datain_b <= x"00000063";
+		t5 =: result -- andi t5,t4,63   Expect 0000001e
 		wait for 20 ns; 
 		-- Test or
 		control <= "01000"; -- opcode "010" for or/i  funct2 "00" for or
-		datain_a <=       ;
-		datain_b <= h(ZZZZZZZZ);
+		datain_a <= t5;
+		datain_b <= t4;
+		t6 =: result -- or t6,t5,t4  Expect 000004de
 		wait for 20 ns; 
 		-- Test ori
 		control <= "01010"; -- opcode "010" for add/sub  funct2 "10" for ori
-		datain_a <=       ;
-		datain_b <= h(ZZZZZZZZ);
+		datain_a <= t6;
+		datain_b <= x"00000238";
+		a0 =: result -- ori a0,t6,568   Expect 000006fe
 		wait for 20 ns; 
 		-- Test sll
 		control <= "01100"; -- opcode "011" for sll/i, srl/i funct2 "00" for sll
-		datain_a <=       ;
-		datain_b <= h(ZZZZZZZZ);
+		datain_a <= a0;
+		datain_b <= t0;
+		a1 =: result; -- sll a1,a0,t0   Expect 00001bf8
 		wait for 20 ns; 
 		-- Test slli
 		control <= "01110"; -- opcode "011" for sll/i, srl/i funct2 "10" for slli
-		datain_a <=       ;
-		datain_b <= h(ZZZZZZZZ);
+		datain_a <= a1;
+		datain_b <= x"0000002";
+		a2 =: result; -- slli a2,a1,2 Expect 00006fe0
 		wait for 20 ns; 
 		-- Test srl 
 		control <= "01101"; -- opcode "011" for sll/i, srl/i funct2 "01" for srl
-		datain_a <=       ;
-		datain_b <= h(ZZZZZZZZ);
+		datain_a <= a2;
+		datain_b <= t0;
+		a3 =: result; -- srl a3,a2,t0   Expect 00001bf8
 		wait for 20 ns; 
 		-- Test srli
 		control <= "01111"; -- opcode "011" for sll/i, srl/i funct2 "11" for srli
-		datain_a <=       ;
-		datain_b <= h(ZZZZZZZZ);
+		datain_a <= a3;
+		datain_b <= x"0000002";
+		a4 =: result; -- srli a4,a3,2 Expect 000006fe
 		wait for 20 ns; 
 		-- Test Data 2 passthrough and zero line
 		control <= "10000"; -- Opcode "100" for d2 pass through funct2 "00" to default
-		datain_a <= h(00000001);
+		datain_a <= a4;
 		datain_b <= h(00000000);
 		-- Wait 100ns to exit
 		wait for 100 ns;
