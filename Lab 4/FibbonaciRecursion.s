01993a3d3f39c3ba2d6e662768329c89c5d57b3a
Samuel <vanness2@seattleu.edu>
2018-05-13 17:57:27 -0700

    Final Update to Fibonacci

--- Lab 4/FibbonaciRecursion.s
+++ Lab 4/FibbonaciRecursion.s
@@ -6,39 +6,48 @@ C: .word 0
 
 .text
 
+addi t1, zero, 1 #initial temp used throughout program
+
 addi a0,zero,3
-add t0, zero, zero
+jal NegativeCheck
 jal Fib_Check
-add t4, a0, zero #Save result to t4 for viewing
+add t4, zero, a1 #display n=3
 
 addi a0,zero,10
-add t0, zero, zero
-jal Fib_Check 
-add t5, a0, zero #Save result to t5 for viewing
+jal NegativeCheck
+jal Fib_Check
+add t5, zero, a1 #display n=10
 
 addi a0,zero,30
-add t0, zero, zero
+jal NegativeCheck
 jal Fib_Check 
-add t6, a0, zero #Save result to t6 for viewing
+add t6, zero, a1 #Display n=30
 
 j Exit
 
+NegativeCheck:
+	blt a0, t1, setNegative
+	ret
+	setNegative:
+		add a0, zero, zero
+		j Fib_Check
+
 Fib_Check:
-	addi t1, zero, 1
+	
 	bgt a0, t1, Fib_Loop #Go to loop if n>1
 	mv a1, a0 
 	ret
 
 Fib_Loop:
 	addi sp, sp, -12
-	sw ra, 0(sp)	#save ra
+	sw ra, 0(sp)	#save return address
 	
 	sw a0, 4(sp)    #save n
 	addi a0, a0, -1 #n-1
 	jal Fib_Check   
 	
 	lw a0, 4(sp)    #restore n
-	sw a1, 8(sp)	#save retval from fib(n-1)
+	sw a1, 8(sp)	#save value from Fib_Check(n-1)
 	
 	addi a0, a0, -2 #n-2
 	jal Fib_Check
@@ -47,8 +56,7 @@ Fib_Loop:
 	add a1, t0, a1 	#our return value
  	
 	lw ra, 0(sp)	#restore ra
-	addi sp, sp, 12 #return stack to normal
+	addi sp, sp, 12 #clean stack
 	ret
 	
-
 Exit:
