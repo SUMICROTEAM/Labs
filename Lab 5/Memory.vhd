1d45c427d4b6623f431c1f9bd9e3c08757bd0366
HunterM982 <38116796+HunterM982@users.noreply.github.com>
2018-05-17 21:44:54 -0700

    Wrote RAM, added 32 bit reg component
    Untested RAM code, added reg32 components to file

--- Lab 5/Memory.vhd
+++ Lab 5/Memory.vhd
@@ -34,14 +34,17 @@ begin
       end loop;
     end if;
 
-    if falling_edge(Clock) then
-	-- Add code to write data to RAM
-	-- Use to_integer(unsigned(Address)) to index the i_ram array
-	
+    if (falling_edge(Clock) AND WE AND Address < b"000000000000000000000111111101") then -- WE in this spot could cause issues, move if no function, Use to_integer(unsigned(Address)) to index the i_ram array
+		i_ram(to_integer(unsigned(Address))) <= DataIn;
     end if;
 
-	-- Rest of the RAM implementation
-
+	if ((OE == 0) AND Address < b"000000000000000000000111111101") then
+		DataOut <= i_ram(to_integer(unsigned(Address)));
+	else
+		DataOut <= x"ZZZZZZZZ";
+	end if;
+	
+	
   end process RamProc;
 
 end staticRAM;	
@@ -75,5 +78,101 @@ begin
     -- Add your code here for the Register Bank implementation
 
 end remember;
-
+-------------------------------------------------------------------------------------------------------------------------------------------------
+-- ALL CODE BELOW IS 32 BIT REGISTER IMPLEMENTATION NO EDITS NEEDED --
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+Library ieee;
+Use ieee.std_logic_1164.all;
+Use ieee.numeric_std.all;
+Use ieee.std_logic_unsigned.all;
+
+entity register32 is
+	port(datain: in std_logic_vector(31 downto 0);
+		 enout32,enout16,enout8: in std_logic;
+		 writein32, writein16, writein8: in std_logic;
+		 dataout: out std_logic_vector(31 downto 0));
+end entity register32;
+
+architecture biggermem of register32 is
+	component register8 
+		port(datain: in std_logic_vector(7 downto 0);
+			enout:  in std_logic;
+			writein: in std_logic;
+			dataout: out std_logic_vector(7 downto 0));
+	end component register8; 
+	signal en8,en16,en32: std_logic := '1';
+	signal write8,write16,write32: std_logic := '0';
+begin
+
+	en32 <= enout32;
+	en16 <= enout32 AND enout16;
+	en8 <= enout32 AND enout16 AND enout8;
+
+	write32 <= writein32;
+	write16 <= writein32 OR writein16;
+	write8 <= writein32 OR writein16 OR writein8;
+
+
+	B1: register8 port map (datain(7 downto 0),en8,write8,dataout(7 downto 0));
+	B2: register8 port map (datain(15 downto 8),en16,write16,dataout(15 downto 8));
+	B3: register8 port map (datain(23 downto 16),en32,write32,dataout(23 downto 16));
+	B4: register8 port map (datain(31 downto 24),en32,write32,dataout(31 downto 24));
+end architecture biggermem;
+---------------------------------------------------------------------------------------------------------------------------------------------------------
+Library ieee;
+Use ieee.std_logic_1164.all;
+Use ieee.numeric_std.all;
+Use ieee.std_logic_unsigned.all;
+
+entity register8 is
+	port(datain: in std_logic_vector(7 downto 0);
+	     enout:  in std_logic;
+	     writein: in std_logic;
+	     dataout: out std_logic_vector(7 downto 0));
+end entity register8;
+
+architecture memmy of register8 is
+
+	component bitstorage
+		port(bitin: in std_logic;
+		 	 enout: in std_logic;
+		 	 writein: in std_logic;
+		 	 bitout: out std_logic);
+	end component;
+	
+	signal i: integer RANGE 0 to 7 := 0;
+	
+begin
+
+GenCommand:	for i in 0 to 7 generate
+		Li: bitstorage port map (datain(i),enout,writein,dataout(i));
+	end generate;
+	
+end architecture memmy;
+---------------------------------------------------------------------------------------------------------------------------------------------------------
+Library ieee;
+Use ieee.std_logic_1164.all;
+Use ieee.numeric_std.all;
+Use ieee.std_logic_unsigned.all;
+
+entity bitstorage is
+	port(bitin: in std_logic;
+		 enout: in std_logic;
+		 writein: in std_logic;
+		 bitout: out std_logic);
+end entity bitstorage;
+
+architecture memlike of bitstorage is
+	signal q: std_logic := '0';
+begin
+	process(writein) is
+	begin
+		if (rising_edge(writein)) then
+			q <= bitin;
+		end if;
+	end process;
+	
+	-- Note that data is output only when enout = 0	
+	bitout <= q when enout = '0' else 'Z';
+end architecture memlike;
+
